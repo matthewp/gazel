0d01caf81725f4698a92ff3e58407556cbbd9a6c
Matthew Phillips <phillipsm2@gmail.com>
2012-02-28 10:26:09 -0500

    Retabbed the Queue prototype.

--- src/queue.js
+++ src/queue.js
@@ -1,32 +1,36 @@
 function Queue() {
 
-};
+}
 
 Queue.prototype = {
-    items: [],
-    results: [],
-    add: function (action) {
-        this.items.push(action);
-    },
-    complete: function () { },
-    flush: function () {
-        var args = Array.prototype.slice.call(arguments);
-        if (args.length > 0) { this.results.push(args); }
-        if (this.items.length > 0) {
-            var action = this.items.shift();
-            action();
-        } else { // Complete, callback.
-            var results = this.results;
-            this.clear();
-            this.complete(results);
-        }
-    },
-    clear: function () {
-        this.items = [];
-        this.results = [];
+  items: [],
+
+  results: [],
+
+  add: function (action) {
+    this.items.push(action);
+  },
+
+  complete: function () { },
+  flush: function () {
+    var args = Array.prototype.slice.call(arguments);
+    if (args.length > 0) { this.results.push(args); }
+    if (this.items.length > 0) {
+      var action = this.items.shift();
+      action();
+    } else { // Complete, callback.
+      var results = this.results;
+      this.clear();
+      this.complete(results);
     }
+  },
+
+  clear: function () {
+    this.items = [];
+    this.results = [];
+  }
 };
 
 Queue.create = function () {
-        return new Queue;
+  return new Queue;
 };
