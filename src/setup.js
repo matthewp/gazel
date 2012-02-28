478a25051539088036998040603c82720a234524
Matthew Phillips <phillipsm2@gmail.com>
2012-02-28 10:28:17 -0500

    Cleaned up initial setup code.

--- src/setup.js
+++ src/setup.js
@@ -2,10 +2,15 @@
 var gazel = gazel || {};
 
 var exists = function (obj) {
-    return typeof obj !== "undefined" && obj != null;
+  return typeof obj !== 'undefined' && obj != null;
 };
 
-window.indexedDB = window.indexedDB || window.mozIndexedDB
-        || window.msIndexedDB || window.webkitIndexedDB || window.oIndexedDB;
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
+window.indexedDB = window.indexedDB
+  || window.mozIndexedDB
+  || window.msIndexedDB
+  || window.webkitIndexedDB
+  || window.oIndexedDB;
+
+window.IDBTransaction = window.IDBTransaction
+  || window.webkitIDBTransaction;
 
