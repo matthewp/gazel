16efaabf18201db6c64fb8ed8070db32a2a8785e
Matthew Phillips <phillipsm2@gmail.com>
2012-02-28 10:24:35 -0500

    Retabbed the return handlers.

--- src/handlers.js
+++ src/handlers.js
@@ -1,13 +1,13 @@
 function complete(func, params) {
-    if (exists(func) && typeof func === "function") {
-        func.apply(null, params);
-    }
+  if (exists(func) && typeof func === "function") {
+    func.apply(null, params);
+  }
 };
 
 function error(e) {
-    gazel._events.forEach(function (item) {
-        if (item.name.toUpperCase() === "ERROR") {
-            item.action(e);
-        }
-    });
+  gazel._events.forEach(function (item) {
+    if (item.name.toUpperCase() === "ERROR") {
+      item.action(e);
+    }
+  });
 };
