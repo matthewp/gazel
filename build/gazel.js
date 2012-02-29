7f03e414a7a031e10750f18ded968da3a3b58da5
Matthew Phillips <phillipsm2@gmail.com>
2012-02-29 10:08:38 -0500

    Added exec function for handling completed chained actions.

--- build/gazel.js
+++ build/gazel.js
@@ -34,10 +34,14 @@ function Client() {
 Client.prototype = {
   chain: [],
 
+  returned: [],
+
   events: { },
 
-  register: function(action) {
+  register: function(action, callback) {
     if(this.chain.length > 0) {
+      callback = this.exec;
+
       this.chain.push(action);
 
       return;
@@ -46,14 +50,43 @@ Client.prototype = {
     action(callback);
   },
 
+  exec: function(callback) {
+    var args = Array.prototype.slice.call(arguments) || [];
+
+    if(args.length === 1 && typeof args[0] === 'function') {
+      this.complete = callback;
+
+      this.chain.forEach(function(action) {
+        action();
+      });
+
+      return;
+    }
+
+    this.returned.push(args);
+
+    if(this.returned.length === this.chain.length) {
+      // We are complete, reset and return.
+      this.complete(this.returned);
+
+      this.complete = null;
+      this.chain = [];
+      this.returned = [];
+    }
+  },
+
   on: function(eventType, action) {
     var event = this.events[eventType] = this.events[eventType] || [];
     event.push(action);
   },
 
+  get: function(key, callback) {
+
+  },
+
   set: function(key, value, callback) {
     this.register(function() {
-      openReadable(function(os) {
+      openWritable(function(os) {
         // TODO do stuff with objectStore
         callback();
       });
--- src/client.js
+++ src/client.js
@@ -5,10 +5,14 @@ function Client() {
 Client.prototype = {
   chain: [],
 
+  returned: [],
+
   events: { },
 
-  register: function(action) {
+  register: function(action, callback) {
     if(this.chain.length > 0) {
+      callback = this.exec;
+
       this.chain.push(action);
 
       return;
@@ -17,9 +21,38 @@ Client.prototype = {
     action(callback);
   },
 
+  exec: function(callback) {
+    var args = Array.prototype.slice.call(arguments) || [];
+
+    if(args.length === 1 && typeof args[0] === 'function') {
+      this.complete = callback;
+
+      this.chain.forEach(function(action) {
+        action();
+      });
+
+      return;
+    }
+
+    this.returned.push(args);
+
+    if(this.returned.length === this.chain.length) {
+      // We are complete, reset and return.
+      this.complete(this.returned);
+
+      this.complete = null;
+      this.chain = [];
+      this.returned = [];
+    }
+  },
+
   on: function(eventType, action) {
     var event = this.events[eventType] = this.events[eventType] || [];
     event.push(action);
+  },
+
+  get: function(key, callback) {
+
   },
 
   set: function(key, value, callback) {
