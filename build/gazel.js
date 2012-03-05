931ab9837a5b161dbf2dd3691af9c2d756f8398c
Matthew Phillips <phillipsm2@gmail.com>
2012-03-05 16:01:37 -0500

    Added stub for incr function.

--- build/gazel.js
+++ build/gazel.js
@@ -267,6 +267,16 @@ Client.prototype = {
     }, callback);
 
     return this;
+  },
+
+  incr: function(key, by, callback) {
+    this.register(function(cb) {
+      this.get(key, function(val) {
+        this.set(key, val + by, cb);
+      });
+    }, callback);
+
+    return this;
   }
 };
 }).call(this);
--- src/client.js
+++ src/client.js
@@ -77,5 +77,15 @@ Client.prototype = {
     }, callback);
 
     return this;
+  },
+
+  incr: function(key, by, callback) {
+    this.register(function(cb) {
+      this.get(key, function(val) {
+        this.set(key, val + by, cb);
+      });
+    }, callback);
+
+    return this;
   }
 };
