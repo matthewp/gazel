04386910a7533c7df8414731821a26196c063904
Matthew Phillips <phillipsm2@gmail.com>
2012-03-15 14:06:48 -0400

    First page commit

--- src/handlers.js
+++
@@ -1,7 +0,0 @@
-function complete(func, params, context) {
-  if (exists(func) && typeof func === "function") {
-    func.apply(context || null, params);
-  }
-};
-
-function error() { }
--- src/request.js
+++
@@ -1,7 +0,0 @@
-function Request() {
-
-}
-
-Request.prototype = {
-
-};
