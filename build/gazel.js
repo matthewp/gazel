fba7f79e13bc1a6ab6726500bf3984bfb0459494
Matthew Phillips <phillipsm2@gmail.com>
2012-03-16 10:09:03 -0400

    setVersion now working in Firefox. fixes #4
    Firefox doesn't allow you to set e.target.result. Insteading creating a fake object with the same signature to use for recursion.

--- build/gazel.js
+++ build/gazel.js
@@ -180,23 +180,24 @@ function openDatabase(onsuccess, onerror) {
       uDb.createObjectStore(gazel.osName);
   };
 
-  req.onsuccess = function (e) {
+  var reqSuccess;
+  reqSuccess = req.onsuccess = function (e) {
     var sDb = e.target.result;
 
     if (sDb.setVersion && Number(sDb.version) !== gazel.version) {
       var dbReq = sDb.setVersion(String(gazel.version));
       dbReq.onsuccess = function (e2) {
-        e.target.result = e2.target.result.db;
-        req.onupgradeneeded(e);
-        req.onsuccess(e);
+        var e3 = {}; e3.target = {};
+        e3.target.result = e2.target.result.db;
+        
+        req.onupgradeneeded(e3);
+        reqSuccess(e3);
         
         return;
       };
 
       dbReq.onerror = onerror;
-
       dbReq.onfailure = onerror;
-
       dbReq.onblocked = onerror;
 
       return;
