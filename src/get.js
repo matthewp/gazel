44a620b0d5de26ee9113d41bd98e4e3f87434d1b
Matthew Phillips <phillipsm2@gmail.com>
2012-03-28 21:26:31 -0400

    Fixes many of the issues with Firefox Nightly. #7
    IDBTransaction.READ_ONLY, not IDBTransaction.READ. Chrome doesn't seem to mind the change either.

--- src/get.js
+++ src/get.js
@@ -4,7 +4,7 @@ Client.prototype.get = function(key, callback) {
   this.register('read', function(uuid, cb) {
     openDatabase(function(db) {
 
-      var tx = self.trans.pull(db, uuid, IDBTransaction.READ);
+      var tx = self.trans.pull(db, uuid, IDBTransaction.READ_ONLY);
 
       var req = tx.objectStore(gazel.osName).get(key);
       req.onerror = self.handleError.bind(self);
