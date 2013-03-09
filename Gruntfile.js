7a1e8b111e7b0271fc2e3962f1a8ab2d636ce795
Matthew Phillips <matthew@matthewphillips.info>
2013-03-08 19:55:25 -0500

    Switching from jake to grunt, to stay with the trends.

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+  var outFile = 'build/gazel.js',
+      minFile = 'build/gazel.min.js';
+
+  var files = [
+    "src/setup.js",
+    "src/uuid.js",
+    "src/dict.js",
+    "src/trans.js",
+    "src/client.js",
+    "src/discard.js",
+    "src/error.js",
+    "src/get.js",
+    "src/set.js",
+    "src/incr.js",
+    "src/decr.js",
+    "src/del.js",
+    "src/print.js",
+    "src/gazel.js",
+    "src/dbfunctions.js"
+  ];
+
+  grunt.initConfig({
+    concat: {
+      dist: {
+        src: files,
+        dest: outFile
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'build/gazel.min.js': outFile
+        }
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js'],
+        options: {
+          es5: true,
+          globals: {
+          }
+        }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('default', [ 'concat', 'uglify']);
+};
